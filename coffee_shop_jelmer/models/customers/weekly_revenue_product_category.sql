6576e5c3a895b6fd300051cb2b68f3019c911e07
jlmr1990 <70751658+jlmr1990@users.noreply.github.com>
2023-05-11 12:10:38 +0000

    changed source in product_category model

--- coffee_shop_jelmer/models/customers/weekly_revenue_product_category.sql
+++ coffee_shop_jelmer/models/customers/weekly_revenue_product_category.sql
@@ -3,8 +3,8 @@ with total_orders as (
         timestamp_trunc(created_at, week) as week_of
         , sum(total) as weekly_total_revenue
         , order_items.product_id
-    from `analytics-engineers-club.coffee_shop.orders` orders
-    left join `analytics-engineers-club.coffee_shop.order_items` order_items on orders.id = order_items.order_id
+    from {{ source('coffee_shop', 'orders')}} orders
+    left join {{ source('coffee_shop', 'order_items')}} order_items on orders.id = order_items.order_id
     group by 1,3
 ),
 
@@ -12,7 +12,7 @@ product_category as (
     select total_orders.weekly_total_revenue
         , total_orders.week_of
         , category as product_category
-    from `analytics-engineers-club.coffee_shop.products` products
+    from {{ source('coffee_shop', 'products')}} products
     left join total_orders on total_orders.product_id = products.id
 )
 
